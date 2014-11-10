6418b278f59846d4a3473539019e5cc8425bbce4
Imageineither <Imageineither@gmail.com>
2014-11-10 07:50:16 -0500

    Scripts I Use

---
+++ Commons/AutoBuyItems_Itemlists.lua
@@ -0,0 +1,204 @@
+Champions = {
+	Ahri = {
+	},
+	Akali = {
+	},
+	Alistar = {
+	},
+	Amumu = {
+		["Default"] = {
+			ItemList1 = {1039,3207,3111,3068,3065,3083,3075,},
+			ItemList2 = {},
+			ItemList3 = {{2003,0,1,1,1,},},
+		},
+	},
+	Anivia = {
+	},
+	Annie = {
+	},
+	Ashe = {
+	},
+	Blitzcrank = {
+	},
+	Brand = {
+	},
+	Caitlyn = {
+	},
+	Cassiopeia = {
+	},
+	Chogath = {
+	},
+	Corki = {
+	},
+	DrMundo = {
+	},
+	Evelynn = {
+	},
+	Ezreal = {
+	},
+	FiddleSticks = {
+	},
+	Fiora = {
+	},
+	Fizz = {
+	},
+	Galio = {
+	},
+	Gangplank = {
+	},
+	Garen = {
+	},
+	Gragas = {
+	},
+	Graves = {
+	},
+	Heimerdinger = {
+	},
+	Irelia = {
+	},
+	Janna = {
+	},
+	JarvanIV = {
+	},
+	Jax = {
+	},
+	Jinx = {
+		["Default"] = {
+			ItemList1 = {1055,1053,3031,3087,3072,3035,3026,},
+			ItemList2 = {},
+			ItemList3 = {{2003,0,5,1,10,},},
+		},
+	},
+	Karma = {
+	},
+	Karthus = {
+	},
+	Kassadin = {
+	},
+	Katarina = {
+	},
+	Kayle = {
+	},
+	Kennen = {
+	},
+	KogMaw = {
+	},
+	Leblanc = {
+	},
+	LeeSin = {
+	},
+	Leona = {
+	},
+	Lux = {
+	},
+	Malphite = {
+	},
+	Malzahar = {
+	},
+	Maokai = {
+	},
+	MasterYi = {
+	},
+	MissFortune = {
+	},
+	MonkeyKing = {
+	},
+	Mordekaiser = {
+	},
+	Morgana = {
+	},
+	Nasus = {
+	},
+	Nautilus = {
+	},
+	Nidalee = {
+	},
+	Nocturne = {
+	},
+	Nunu = {
+	},
+	Olaf = {
+	},
+	Orianna = {
+	},
+	Pantheon = {
+	},
+	Poppy = {
+	},
+	Rammus = {
+	},
+	Renekton = {
+	},
+	Riven = {
+	},
+	Rumble = {
+	},
+	Ryze = {
+	},
+	Sejuani = {
+	},
+	Shaco = {
+	},
+	Shen = {
+	},
+	Shyvana = {
+	},
+	Singed = {
+	},
+	Sion = {
+	},
+	Sivir = {
+	},
+	Skarner = {
+	},
+	Sona = {
+	},
+	Soraka = {
+	},
+	Swain = {
+	},
+	Talon = {
+	},
+	Taric = {
+	},
+	Teemo = {
+	},
+	Tristana = {
+	},
+	Trundle = {
+	},
+	Tryndamere = {
+	},
+	TwistedFate = {
+	},
+	Twitch = {
+	},
+	Udyr = {
+	},
+	Urgot = {
+	},
+	Varus = {
+	},
+	Vayne = {
+	},
+	Veigar = {
+	},
+	Viktor = {
+	},
+	Vladimir = {
+	},
+	Volibear = {
+	},
+	Warwick = {
+	},
+	Xerath = {
+	},
+	XinZhao = {
+	},
+	Yorick = {
+	},
+	Ziggs = {
+	},
+	Zilean = {
+	},
+}
